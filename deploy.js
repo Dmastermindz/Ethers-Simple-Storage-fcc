d0bbfa39d042aa2c1f94ac1010c2f8ca7efbc455
Dmastermindz <cclarke98@gmail.com>
2023-03-12 03:53:46 +0500

    v2

--- deploy.js
+++ deploy.js
@@ -10,22 +10,23 @@ async function main() {
     console.log(process.env.RPC_URL);
 
     const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-    // const wallet = new ethers.Wallet(
-    //     process.env.PRIVATE_KEY,
-    //     provider
-    // );
-    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
-    let wallet = new ethers.Wallet.fromEncryptedJsonSync(
-        encryptedJson, 
-        process.env.PRIVATE_KEY_PASSWORD
-        );
-    wallet = await wallet.connect(provider);
+    const wallet = new ethers.Wallet(
+        process.env.PRIVATE_KEY,
+        provider
+    );
+    // const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+    // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+    //     encryptedJson, 
+    //     process.env.PRIVATE_KEY_PASSWORD
+    //     );
+    //wallet = await wallet.connect(provider);
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
     console.log("Deploying, pelase wait...");
     const contract = await contractFactory.deploy(); //STOP Here wait for contract to deploy
-    await contract.deployTransaction.wait(1); 
+    await contract.deployTransaction.wait(1);
+    console.log(`Contract Address: ${contract.address}`);
 
 
     const currentFavoriteNumber = await contract.retrieve();
